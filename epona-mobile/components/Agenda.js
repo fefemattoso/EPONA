88c964c8ddd47761152467acc369ab4ecc2bd47c
BuenoOtavio <buenootaviolima@gmail.com>
2024-09-20 11:28:39 -0300

    Mobile

--- epona-mobile/components/Agenda.js
+++ epona-mobile/components/Agenda.js
@@ -1,102 +1,257 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, FlatList, TouchableOpacity, Animated, Switch, ScrollView } from 'react-native';
+import {LocaleConfig} from 'react-native-calendars';
+import React, {useState, useEffect} from 'react';
+import {Calendar} from 'react-native-calendars';
+import {
+  View, 
+  Text, 
+  Modal, 
+  Button, 
+  TextInput, 
+  StyleSheet, 
+  TouchableOpacity
+} from 'react-native';
+import {db} from '../firebaseconfig';
+import {collection, addDoc, getDocs, doc, updateDoc, deleteDoc} from 'firebase/firestore';
 
-export default function App() {
-  const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
-  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
-  const [days, setDays] = useState([]);
-  const [weekdays, setWeekdays] = useState(['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']);
+LocaleConfig.locales['br'] = {
+  monthNames: [
+    'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+    'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+  ],
+  monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+  dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+  dayNamesShort: ['Dom.', 'Seg.', 'Ter.', 'Qua.', 'Qui', 'Sex.', 'Sab.'],
+  today: 'Hoje'
+};
+
+LocaleConfig.defaultLocale = 'br';
+
+const App = () => {
+  const [selected, setSelected] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
+  const [descricao, setDescricao] = useState('');
+  const [markedDates, setMarkedDates] = useState({});
+  const [modalDescriptionVisible, setModalDescriptionVisible] = useState(false);
+  const [selectedDescription, setSelectedDescription] = useState('');
+  const [selectedDateId, setSelectedDateId] = useState(null); // Para armazenar o ID do documento no Firebase
 
   useEffect(() => {
-    const getDaysInMonth = (month, year) => {
-      const daysInMonth = [];
-      const firstDayOfMonth = new Date(year, month, 1);
-      const lastDayOfMonth = new Date(year, month + 1, 0);
+    fetchDatesFromFirebase();
+  }, []);
 
-      for (let i = 0; i < firstDayOfMonth.getDay(); i++) {
-        daysInMonth.push('');
-      }
-
-      for (let i = 1; i <= lastDayOfMonth.getDate(); i++) {
-        daysInMonth.push(i);
-      }
-
-      return daysInMonth;
-    };
-
-    setDays(getDaysInMonth(currentMonth, currentYear));
-  }, [currentMonth, currentYear]);
-
-  const handleNextMonth = () => {
-    if (currentMonth === 11) {
-      setCurrentMonth(0);
-      setCurrentYear(currentYear + 1);
-    } else {
-      setCurrentMonth(currentMonth + 1);
+  // Função para buscar datas salvas no Firebase e marcar no calendário
+  const fetchDatesFromFirebase = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, 'Dates'));
+      const marked = {};
+      querySnapshot.forEach(docSnapshot => {
+        const {date, descricao} = docSnapshot.data();
+        marked[date] = {
+          marked: true, 
+          selected: false, 
+          customStyles: {
+            container: {backgroundColor: 'green'}, 
+            text: {color: 'white'}
+          }, 
+          descricao,
+          id: docSnapshot.id // Armazena o ID do documento do Firebase
+        };
+      });
+      setMarkedDates(marked);
+    } catch (e) {
+      console.error('Erro ao buscar as datas: ', e);
     }
   };
 
-  const handlePreviousMonth = () => {
-    if (currentMonth === 0) {
-      setCurrentMonth(11);
-      setCurrentYear(currentYear - 1);
+  const handleDayPress = (day) => {
+    const dayKey = day.dateString;
+    
+    if (markedDates[dayKey]) {
+      // Exibir o modal com a descrição da data cadastrada
+      setSelectedDescription(markedDates[dayKey].descricao);
+      setSelectedDateId(markedDates[dayKey].id); // Armazena o ID do documento
+      setModalDescriptionVisible(true);
     } else {
-      setCurrentMonth(currentMonth - 1);
+      // Abrir modal para adicionar nova descrição
+      setSelected(dayKey);
+      setModalVisible(true);
+    }
+  };
+
+  const saveDateToFirebase = async () => {
+    try {
+      const docRef = await addDoc(collection(db, 'Dates'), {
+        date: selected,
+        descricao: descricao
+      });
+
+      // Atualizar a data marcada com cor diferenciada
+      setMarkedDates({
+        ...markedDates,
+        [selected]: {
+          marked: true, 
+          customStyles: {container: {backgroundColor: 'green'}, text: {color: 'white'}}, 
+          descricao,
+          id: docRef.id // Salva o ID do documento no estado
+        }
+      });
+
+      alert('Data cadastrada com sucesso!');
+      setModalVisible(false);
+      setDescricao('');
+    } catch (e) {
+      console.error("Erro ao salvar a data: ", e);
+    }
+  };
+
+  // Função para editar a descrição da data no Firebase
+  const updateDescriptionInFirebase = async () => {
+    try {
+      const docRef = doc(db, 'Dates', selectedDateId);
+      await updateDoc(docRef, {
+        descricao: selectedDescription
+      });
+
+      // Atualizar a descrição no estado
+      setMarkedDates({
+        ...markedDates,
+        [selected]: {
+          ...markedDates[selected],
+          descricao: selectedDescription
+        }
+      });
+
+      alert('Descrição atualizada com sucesso!');
+      setModalDescriptionVisible(false);
+    } catch (e) {
+      console.error("Erro ao atualizar a descrição: ", e);
+    }
+    fetchDatesFromFirebase();
+  };
+
+  // Função para excluir a data do Firebase e do calendário
+  const deleteDateFromFirebase = async () => {
+    try {
+      const docRef = doc(db, 'Dates', selectedDateId);
+      await deleteDoc(docRef);
+
+      const updatedMarkedDates = {...markedDates};
+      delete updatedMarkedDates[selected]; // Remove a data do estado local
+
+      setMarkedDates(updatedMarkedDates);
+      alert('Data excluída com sucesso!');
+      setModalDescriptionVisible(false);
+    } catch (e) {
+      console.error("Erro ao excluir a data: ", e);
     }
   };
 
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.month}>{getMonthName(currentMonth)} {currentYear}</Text>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity onPress={handlePreviousMonth}>
-            <Text style={styles.buttonText}>Previous</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={handleNextMonth}>
-            <Text style={styles.buttonText}>Next</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-      <View style={styles.weekdaysContainer}>
-        {weekdays.map((day, index) => (
-          <Text key={index} style={styles.weekdayText}>{day}</Text>
-        ))}
-      </View>
-      <FlatList
-        data={days}
-        renderItem={({ item, index }) => (
-          <View style={styles.dayContainer}>
-            {item ? (
-              <Text style={styles.dayText}>{item}</Text>
-            ) : (
-              <View style={styles.emptyDay} />
-            )}
-          </View>
-        )}
-        numColumns={7}
-        keyExtractor={(item, index) => index.toString()}
+      <Calendar
+        style={{
+          borderWidth: 1,
+          borderColor: 'gray',
+          height: 350,
+        }}
+        theme={{
+          backgroundColor: '#ffffff',
+          calendarBackground: '#ffffff',
+          textSectionTitleColor: '#b6c1cd',
+          selectedDayBackgroundColor: '#00adf5',
+          selectedDayTextColor: '#ffffff',
+          todayTextColor: '#00adf5',
+          dayTextColor: '#2d4150',
+          textDisabledColor: '#d9e1e8'
+        }}
+        onDayPress={handleDayPress}
+        markedDates={markedDates}
+        markingType={'custom'}
       />
+
+      {/* Modal para adicionar descrição */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Cadastrar Data</Text>
+            <Text style={styles.label}>Data Selecionada: {selected}</Text>
+
+            <TextInput
+              style={styles.input}
+              placeholder="Digite a descrição"
+              value={descricao}
+              onChangeText={setDescricao}
+            />
+
+            <Button
+              title="Salvar Data"
+              onPress={saveDateToFirebase}
+              color="#9370db"
+            />
+            
+            <TouchableOpacity
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(false)}
+            >
+              <Text style={styles.textStyle}>Fechar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+
+      {/* Modal para exibir e editar a descrição da data cadastrada */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalDescriptionVisible}
+        onRequestClose={() => {
+          setModalDescriptionVisible(!modalDescriptionVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Descrição da Data</Text>
+
+            {/* Campo para editar a descrição */}
+            <TextInput
+              style={styles.input}
+              placeholder="Edite a descrição"
+              value={selectedDescription}
+              onChangeText={setSelectedDescription}
+            />
+
+            {/* Botão para salvar a nova descrição */}
+            <Button
+              title="Atualizar Descrição"
+              onPress={updateDescriptionInFirebase}
+              color="#9370db"
+            />
+
+            {/* Botão para excluir a data */}
+            <Button
+              title="Excluir Data"
+              onPress={deleteDateFromFirebase}
+              color="#ff6347"
+            />
+
+            <TouchableOpacity
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalDescriptionVisible(false)}
+            >
+              <Text style={styles.textStyle}>Fechar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
-};
-
-const getMonthName = (month) => {
-  const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
-  return months[month];
 };
 
 const styles = StyleSheet.create({
@@ -104,47 +259,52 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
   },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 20,
-  },
-  month: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-  },
-  buttonText: {
-    fontSize: 16,
-    marginHorizontal: 10,
-  },
-  weekdaysContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 10,
-  },
-  weekdayText: {
-    fontSize: 16,
-    width: 30,
-    textAlign: 'center',
-  },
-  dayContainer: {
-    width: 30,
-    height: 30,
+  centeredView: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    margin: 5,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
-  dayText: {
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  input: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    marginVertical: 10,
+    width: 200,
+  },
+  label: {
     fontSize: 16,
-    textAlign: 'center',
+    marginBottom: 10,
   },
-  emptyDay: {
-    width: 30,
-    height: 30,
-    backgroundColor: '#f0f0f0',
+  modalText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 15,
   },
+  buttonClose: {
+    backgroundColor: '#ff6347',
+    marginTop: 10,
+  },
+  textStyle: {
+    color: 'white',
+    padding: 10,
+  }
 });
+
+export default App;
--- epona-mobile/components/Login.js
+++ epona-mobile/components/Login.js
@@ -1,54 +1,89 @@
 import React, { useState } from 'react';
-import { View, Text, Button, Image, StyleSheet } from 'react-native';
-// import './Login.css';
-
+import { StyleSheet, Text, View, Button, TextInput, Image } from 'react-native';
+import { auth } from '../firebaseconfig'; // Certifique-se de configurar corretamente o auth no firebaseconfig.js
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
 function Login({ onLogin }) {
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  
-  const handleLogin = () => {
-    if (username && password) {
-      onLogin(username);
+  const [isRegistering, setIsRegistering] = useState(false); // Estado para alternar entre cadastro e login
+
+  const handleLogin = async () => {
+    if (email && password) {
+      try {
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        onLogin(userCredential.user); // Login bem-sucedido
+      } catch (error) {
+        alert("Erro ao realizar login: " + error.message);
+      }
     } else {
       alert("Por favor, insira suas credenciais.");
     }
   };
 
+  const handleRegister = async () => {
+    if (email && password) {
+      if (password.length < 6) {
+        alert("A senha deve ter no mínimo 6 caracteres.");
+        return;
+      }
+
+      try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        onLogin(userCredential.user); // Cadastro bem-sucedido e login automático
+      } catch (error) {
+        alert("Erro ao cadastrar!");
+      }
+    } else {
+      alert("Por favor, insira um email e senha válidos.");
+    }
+  };
+
   return (
     <View style={styles.loginContainer}>
-      <Text style={styles.title}>Login no Epona</Text>
-      <input style={styles.input}
-        type="text" 
-        placeholder="Usuário" 
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
+      <Text style={styles.title}>{isRegistering ? "Cadastrar" : "Login"} no Epona</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
       />
-      <input style={styles.input}
-        type="password" 
+      <TextInput
+        style={styles.input}
         placeholder="Senha"
         value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        onChangeText={setPassword}
+        secureTextEntry
       />
       <View style={styles.buttons}>
-        <Button style={styles.button} color='#162040' title="Entrar" onPress={handleLogin} />
+        {isRegistering ? (
+          <>
+            <Button style={styles.button} color='#162040' title="Cadastrar" onPress={handleRegister} />
+            <Button style={styles.button} color='#162040' title="Voltar para Login" onPress={() => setIsRegistering(false)} />
+          </>
+        ) : (
+          <>
+            <Button style={styles.button} color='#162040' title="Entrar" onPress={handleLogin} />
+            <Button style={styles.button} color='#162040' title="Cadastrar" onPress={() => setIsRegistering(true)} />
+          </>
+        )}
         <Button style={styles.button} color='#162040' title="Entrar como Visitante" onPress={() => onLogin('guest')} />
-        <Button style={styles.button} color='#162040' title="Cadastrar" onPress={() => alert('Cadastro ainda não disponível!')} />
       </View>
       <Image source={require('../assets/planta2.png')} style={styles.leafTopLeft} />
       <Image source={require('../assets/planta.png')} style={styles.leafBottomRight} />
     </View>
   );
-  
 }
 
 const styles = StyleSheet.create({
   loginContainer: {
-    flex: 1, // Faz o contêiner ocupar toda a tela
-    justifyContent: 'center', // Centraliza verticalmente
-    alignItems: 'center', // Centraliza horizontalmente
-    backgroundColor: '#badda8', // Mantém a cor de fundo
-    padding: 20, // Adiciona o padding para espaçamento interno
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#badda8',
+    padding: 20,
     borderRadius: 8,
   },
   title: {
@@ -67,11 +102,9 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   buttons: {
-    flexDirection: 'row',
+    flexDirection: 'column',
     gap: 20,
     marginBottom: 20,
-    display: 'flex',
-    flexDirection: 'column',
   },
   button: {
     backgroundColor: '#162040',
@@ -96,7 +129,5 @@ const styles = StyleSheet.create({
     height: 150,
   },
 });
-
-
 
 export default Login;
