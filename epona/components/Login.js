f3ff46c9d110636ee3af1ee691fc4ae8b7e67c87
fefemattoso <mfernandamattoso@gmail.com>
2024-09-26 11:22:46 -0300

    mobile

---
+++ epona/components/Login.js
@@ -0,0 +1,145 @@
+import React, { useState } from 'react';
+import { View, Text, Button, TextInput, Image, StyleSheet } from 'react-native';
+import { auth } from '../firebaseconfig';
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
+
+function Login({ onLogin, onGoBack }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isRegistering, setIsRegistering] = useState(false);
+
+  const handleLogin = () => {
+    if (email && password) {
+      signInWithEmailAndPassword(auth, email, password)
+        .then(() => {
+          console.log('Usuário logado com sucesso');
+          onLogin(email);
+        })
+        .catch((error) => {
+          console.error('Erro ao fazer login: ', error);
+          alert('Falha no login, verifique suas credenciais.');
+        });
+    } else {
+      alert('Por favor, insira suas credenciais.');
+    }
+  };
+
+  const handleRegister = () => {
+    if (email && password) {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then(() => {
+          console.log('Usuário cadastrado com sucesso');
+          alert('Usuário cadastrado com sucesso!');
+          setIsRegistering(false);  // Volta para tela de login após cadastro
+        })
+        .catch((error) => {
+          console.error('Erro ao fazer cadastro: ', error);
+          alert('Falha no cadastro, verifique suas informações.');
+        });
+    } else {
+      alert('Por favor, insira suas credenciais.');
+    }
+  };
+
+  return (
+    <View style={styles.loginContainer}>
+      <Text style={styles.title}>{isRegistering ? 'Cadastrar' : 'Login'} no Epona</Text>
+
+      <View style={styles.formContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Senha"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+
+        <View style={styles.buttons}>
+          {isRegistering ? (
+            <>
+              <Button color='#162040' title="Cadastrar" onPress={handleRegister} />
+              <Button color='#162040' title="Voltar para Login" onPress={() => setIsRegistering(false)} />
+            </>
+          ) : (
+            <>
+              <Button color='#162040' title="Entrar" onPress={handleLogin} />
+              <Button color='#162040' title="Cadastrar" onPress={() => setIsRegistering(true)} />
+            </>
+          )}
+        </View>
+      </View>
+
+
+      <Image source={require('../assets/planta2.png')} style={styles.leafTopLeft} />
+      <Image source={require('../assets/planta.png')} style={styles.leafBottomRight} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  loginContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#badda8',
+    position: 'relative',
+    padding: 10,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#162040',
+    marginBottom: 30,
+  },
+  formContainer: {
+    width: '85%',
+    maxWidth: 400,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 10,
+    padding: 25,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 6,
+    elevation: 5,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#547699',
+    borderRadius: 8,
+    padding: 10,
+    marginVertical: 10,
+    width: '100%',
+    fontSize: 16,
+  },
+  buttons: {
+    marginTop: 20,
+    gap: 10,
+  },
+  leafTopLeft: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: 100,
+    height: 100,
+  },
+  leafBottomRight: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    width: 100,
+    height: 100,
+    marginRight: 30,
+  },
+});
+
+export default Login;
---
+++ epona/components/Welcome.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { View, Text, Button, Image, StyleSheet } from 'react-native';
+import { getAuth, signInAnonymously } from 'firebase/auth';
+
+const Welcome = ({ onContinue, onVisitorAccess }) => {
+  // Função para autenticação anônima
+  const handleVisitorAccess = () => {
+    const auth = getAuth();
+    signInAnonymously(auth)
+      .then(() => {
+        onVisitorAccess();
+      })
+      .catch((error) => {
+        console.error("Erro ao entrar como visitante: ", error);
+      });
+  };
+
+  return (
+    <View style={styles.welcomeContainer}>
+      <Image source={require('../assets/planta2.png')} style={styles.leafTopLeft} />
+      <Text style={styles.title}>Bem-vindo ao Epona</Text>
+      <Image source={require('../assets/icon.png')} style={styles.icon} />
+      <Text style={styles.subtitle}>Seu assistente pessoal para gerenciar tarefas e lembretes</Text>
+
+      <View style={styles.buttonContainer}>
+        {/* Botão para ir à tela de login */}
+        <Button
+          style={styles.button}
+          color='#162040'
+          title="Login"
+          onPress={onContinue}
+        />
+      </View>
+
+      {/* Folha decorativa na parte inferior */}
+      <Image source={require('../assets/planta.png')} style={styles.leafBottomRight} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  welcomeContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#badda8', // Cor da paleta
+    position: 'relative',
+    textAlign: 'center',
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: '#162040', // Cor escura da paleta
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#547699', // Azul da paleta
+    marginBottom: 20,
+    textAlign: 'center',
+    paddingHorizontal: 20,
+  },
+  buttonContainer: {
+    marginBottom: 30,
+    width: '60%',
+  },
+  button: {
+    backgroundColor: '#162040', // Cor do botão conforme a paleta
+    padding: 10,
+    borderRadius: 5,
+  },
+  icon: {
+    width: 120,
+    height: 120,
+    marginBottom: 20,
+  },
+  leafTopLeft: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: 130,
+    height: 130,
+  },
+  leafBottomRight: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    width: 130,
+    height: 130,
+  },
+});
+
+export default Welcome;
