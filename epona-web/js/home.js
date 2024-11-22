825c53c42a778ff551a6028e561259b25542ede7
BuenoOtavio <buenootaviolima@gmail.com>
2024-11-22 11:20:32 -0300

    Options

--- epona-web/js/home.js
+++ epona-web/js/home.js
@@ -1,5 +1,61 @@
 document.addEventListener('DOMContentLoaded', () => {
     const sair = document.getElementById("sair");
+    const userName = document.getElementById("usuario");
+    const userOptions = document.getElementById("user-options");
+    const closeBtn = document.querySelector(".close-btn");
+    const darkModeIcon = document.getElementById("dark-mode-icon");
+    const termsLink = document.querySelector('#user-options li a[href="./termos.html"]');
+    const termsModal = document.getElementById('terms-modal');
+    const closeModal = document.querySelector('.close-modal');
+
+    // Abrir o modal
+    termsLink.addEventListener('click', (e) => {
+        e.preventDefault(); // Previne o redirecionamento padrão
+        termsModal.classList.remove('hidden');
+    });
+
+    // Fechar o modal
+    closeModal.addEventListener('click', () => {
+        termsModal.classList.add('hidden');
+    });
+
+    // Fechar o modal ao clicar fora dele
+    window.addEventListener('click', (e) => {
+        if (e.target === termsModal) {
+            termsModal.classList.add('hidden');
+        }
+    });
+
+    const moonIcon = "../images/moon.png"; // Caminho do ícone da lua
+    const sunIcon = "../images/sun.png";
+    // Mostrar o menu lateral
+    userName.addEventListener("click", () => {
+        userOptions.classList.add("active");
+        userOptions.classList.remove("hidden");
+    });
+
+    // Fechar o menu lateral
+    closeBtn.addEventListener("click", () => {
+        userOptions.classList.add("hidden");
+        userOptions.classList.remove("active");
+    });
+
+    darkModeIcon.addEventListener("click", () => {
+        const isDarkMode = document.body.classList.toggle("dark-mode");
+        localStorage.setItem("darkMode", isDarkMode ? "enabled" : "disabled");
+        darkModeIcon.src = isDarkMode ? sunIcon : moonIcon; // Alterar o ícone
+        darkModeIcon.alt = isDarkMode ? "Modo Claro" : "Modo Escuro";
+    });
+
+    // Carregar o estado do modo escuro e o ícone correspondente
+    if (localStorage.getItem("darkMode") === "enabled") {
+        document.body.classList.add("dark-mode");
+        darkModeIcon.src = sunIcon; // Ícone do sol para modo escuro
+        darkModeIcon.alt = "Modo Claro";
+    } else {
+        darkModeIcon.src = moonIcon; // Ícone da lua para modo claro
+        darkModeIcon.alt = "Modo Escuro";
+    }
 
 
     sair.addEventListener("click", () => {
