4223cdb1e7b96feb1d05f101a59b4822d397c503
fefemattoso <mfernandamattoso@gmail.com>
2024-11-29 11:20:03 -0300

    mobile

--- epona/babel.config.js
+++ epona/babel.config.js
@@ -5,8 +5,3 @@ module.exports = function(api) {
   };
 };
 
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'], // Adicione isso
-};
-
